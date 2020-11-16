9e44c6546f8504b84be14069d56ecb83d9721fa9
canxiang.zhao <canxiang.zhao@seek4paws.com>
2020-11-16 17:05:56 +0800

    add NProgress

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,5 @@
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
 import Vue from "vue";
 import VueRouter from "vue-router";
 import NotFound from "../views/NotFound";
@@ -106,6 +108,15 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  next();
+});
+
+router.afterEach(() => {
+  NProgress.done();
 });
 
 export default router;
