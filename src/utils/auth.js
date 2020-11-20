77684798abd515a396a46602320980b474700aae
canxiang.zhao <canxiang.zhao@seek4paws.com>
2020-11-20 17:17:42 +0800

    add auth.js

---
+++ src/utils/auth.js
@@ -0,0 +1,16 @@
+// 获取当前用户角色
+export function getAuthority() {
+  return ["admin"];
+}
+
+// 判断当前用户角色是否存在当前路由的权限数组中
+export function check(authority) {
+  const current = getAuthority();
+  return current.some(item => authority.includes(item));
+}
+
+// 判断用户是否登陆
+export function isLogin() {
+  const current = getAuthority();
+  return current && current[0] !== "guest";
+}
